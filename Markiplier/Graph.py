b2cb88ef9e4329cc88f10a6dd5df74bb1c2a3c68
Samyuth <samyuth.sagi@gmail.com>
2022-05-02 13:52:14 -0400

    Commenting out unnecessary lib

--- Markiplier/Graph.py
+++ Markiplier/Graph.py
@@ -7,7 +7,7 @@ Created on Fri Mar 18 15:50:22 2022
 
 import networkx as nx
 import matplotlib.pyplot as plt
-from pyvis.network import Network
+# from pyvis.network import Network
 import numpy as np
 import json
 
@@ -15,7 +15,7 @@ class Graph:
     # Constructor
     def __init__(self):
         self.graph = nx.MultiDiGraph()
-        self.network = Network()
+        # self.network = Network()
     
     # Method to plot digraph with matplotlib
     def plot(self, figsize):
@@ -23,19 +23,19 @@ class Graph:
         plt.figure(3,figsize=figsize)
         nx.draw(self.graph, pos, with_labels=True)
     
-    # Method to plot digraph with pyvis
-    def plot_pretty(self):
-        net = Network()
-        net.from_nx(self.graph)
-        net.show("visual.html")
-        self.network = net
+    # # Method to plot digraph with pyvis
+    # def plot_pretty(self):
+    #     net = Network()
+    #     net.from_nx(self.graph)
+    #     net.show("visual.html")
+    #     self.network = net
     
-    # Method to output the network x json
-    def output_network_json(self):
-        data = dict()
-        data["nodes"] = self.network.nodes
-        data["edges"] = self.network.edges
-        with open("network.json", "w") as outfile:
-            outfile.write(json.dumps(data, indent=4))
-        with open("../Node-UI/network.json", "w") as outfile:
-            outfile.write(json.dumps(data, indent=4))
+    # # Method to output the network x json
+    # def output_network_json(self):
+    #     data = dict()
+    #     data["nodes"] = self.network.nodes
+    #     data["edges"] = self.network.edges
+    #     with open("network.json", "w") as outfile:
+    #         outfile.write(json.dumps(data, indent=4))
+    #     with open("../Node-UI/network.json", "w") as outfile:
+    #         outfile.write(json.dumps(data, indent=4))
