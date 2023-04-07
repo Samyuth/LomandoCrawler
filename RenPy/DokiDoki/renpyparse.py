22b47c60aae07d25e21579539c27ef3e9f5e3289
cstecc <46802772+cstecc@users.noreply.github.com>
2023-04-07 16:40:57 -0700

    Add files via upload

--- RenPy/DokiDoki/renpyparse.py
+++ RenPy/DokiDoki/renpyparse.py
@@ -1,67 +1,96 @@
-from Graph import *
 import re
+from Graph import *
+import json
 
-graph = nx.MultiDiGraph()
-
-f = open("RenPy\\Doki Doki Literature Club\\"NAME OF DECOMPILED GAME SCRIPT", "r")
-visited = []
-branchlist = []
-choice_dictionary = {}
-prevline = ""
-for line in f:
-    
-    if prevline != "":
-        line = prevline
-        prevline = ""
-    splitline = line.split(" ")
-    #print(splitline)
-    if splitline[0] in "label":
-        nodename = splitline[1].strip()
-        nodename = nodename[:-1]
-        #print(nodename)
-        if nodename not in visited:
-            graph.add_node(nodename)
-            visited.append(nodename)
-        for branch in branchlist:
-            print(branch)
-            graph.add_edge(branch, nodename)
-        choice_dictionary[nodename] = set()
-        loop = True
-        branchlist = []
-        while(loop == True):
-            line = f.readline()
-            if "jump_out" in line:
-                jumpline = line.split("jump_out")
-                secondnode = jumpline[1][1:].strip()
-                #print(secondnode)
-                choice_dictionary[nodename].add(secondnode)
-                if secondnode not in visited and "restart" not in secondnode:
-                    graph.add_node(secondnode)
-                    visited.append(secondnode)
-                #print("{} | {}".format(nodename, secondnode))
-                if "restart" not in secondnode:
-                    graph.add_edge(nodename, secondnode)
-            elif "iscene" in line:
-                isceneline = line.split("iscene")
-                secondnode = isceneline[1].strip()
-                secondnode = re.findall('"([^"]*)"', secondnode)[0]
-                #print(secondnode)
-                choice_dictionary[nodename].add(secondnode)
-                if secondnode not in visited:
-                    graph.add_node(secondnode)
-                    visited.append(secondnode)
-                if(secondnode != nodename):
-                    #print("{} | {}".format(nodename, secondnode))
-                    graph.add_edge(nodename, secondnode)
-                    branchlist.append(secondnode)
-            if "label" in line:
-                loop = False
-            if "# Decompiled by unrpyc:" in line:
-                loop = False
-        prevline = line
+class TextNode():
+    def __init__(self, label=None, call=None,jump=None,text=None, children=None):
+        if label is not None:
+            self.label = label
+        else:
+            self.label = None
+        if call is not None:
+            self.call = call
+        else:
+            self.call = None
+        if jump is not None:
+            self.jump = jump
+        else:
+            self.jump = None
+        if text is not None:
+            self.text = text
+        else:
+            self.text = ""
         
-#print(choice_dictionary)
-actual_graph = Graph(graph)
-#print(visited)
-actual_graph.plot_pretty()
-#print(actual_graph)
+        if children is not None:
+            self.children = children
+        else:
+            self.children = []
+            
+    def get_text(self):
+        if self.text:
+            return self.text
+        else:
+            return None
+
+    def get_label(self):
+        if self.label:
+            return self.label
+        else:
+            return None
+    def get_call(self):
+        if self.call:
+            return self.call
+        else:
+            return None
+    def get_jump(self):
+        if self.jump:
+            return self.jump
+        else:
+            return None
+    def add_text(self, text):
+        self.text += text
+
+    def change_label(self, label):
+        self.label = label
+    def change_call(self, call):
+        self.call = call
+    def change_jump(self, jump):
+        self.jump = jump
+    def add_children(self, children):
+        self.children += children
+
+class ChoiceNode(TextNode):
+    def add_choices(self, choices):
+        self.choices = choices
+    
+    def get_choices(self):
+        if self.choices:
+            return self.choices
+        else:
+            return None
+
+class DokiDokiNode(TextNode):
+    def get_labels(self, string):
+        return re.findall("\*.*(?=,)|\*.*(?=\s)|\*.*", string)
+    
+    def parse_text(self):
+        if self.text is None:
+            print("No text to parse")
+            return -1
+        
+        line_ctr = 0
+        lines = self.text.splitlines()
+        no_lines = len(lines)
+        while (line_ctr < no_lines):
+            if lines[line_ctr].find("select") != -1:
+                children = []   
+                while (line_ctr < no_lines
+                       and re.search("`[0-9].*`", lines[line_ctr])):
+                    children += self.get_labels(lines[line_ctr])
+                    line_ctr += 1
+                self.add_children(children)
+            elif lines[line_ctr].find("goto") != -1:
+                self.add_children(self.get_labels(lines[line_ctr]))
+            
+            line_ctr += 1
+            
